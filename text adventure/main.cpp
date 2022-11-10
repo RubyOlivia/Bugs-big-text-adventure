8a4dbdf601ad55b4a6f65271653c910aeb839dc3
RubyOlivia <rubycannon1990@gmail.com>
2022-11-10 12:02:13 -0500

    initial commit

--- text adventure/main.cpp
+++ text adventure/main.cpp
@@ -1,14 +1,29 @@
 //
 //  main.cpp
 //  text adventure
-//
-//  Created by Ruby on 11/10/22.
-//
+
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::cout << "              --------------------------         \n";
+    std::cout << "    ^    ^    | Bugs big Text-Adventure|   ^ ^    \n";
+    std::cout << "   ( O ^ O)   |  *   *   *   *   *   * |  (* * )   \n";
+    std::cout << "              --------------------------\n\n";
+
+    std::cout << "Bug is a scaredy cat\n";
+    std::cout << "It's kind of pathetic but he's awfully cute\n\n";
+    std::cout << "This is his story\n";
+    
+    char cont = 'y';
+    int bugs_choice = 0;
+    
+    std::cout << " ---------------------------------------------------------\n";
+    std::cout << "Once upon a time, there was a kitten named Bug\n";
+    std::cout << "Or at least he looked like a kitten, he's fully grown but extra small\n";
+    std::cout << "Bug had a brother named Cretin, and two moms; Gaby and Ruby\n";
+    
+    
     return 0;
 }
